2d172a0352aff9a9393c436616b1c7afed15bdf9
Evandro Murilo <evandromurilob@hotmail.com>
2017-03-11 14:56:19 -0400

    generate_grammar.c revamp

--- src/generate_grammar.c
+++ src/generate_grammar.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "grammar_parser.h"
+
+bool isPunctuation(char c);
+void print_expansion(struct StringList *strs);
 
 int main(int argc, char **argv) {
 	if (argc == 1) {
@@ -21,9 +25,44 @@ int main(int argc, char **argv) {
 	strs.last = NULL;
 
 	expand(grammar, get_random_production(start), &strs);
-	print_linked(&strs);
+	print_expansion(&strs);
 	putchar('\n');
 
 	return 0;
+}
+
+#define MAX_LINE_LENGTH 80
+void print_expansion(struct StringList *strs) {
+	struct StringNode *curr = strs->first;
+	int l_length = 0;
+
+	while (curr != NULL) {
+		int w_length = len(curr->value);
+
+		if (w_length == 1 && isPunctuation(curr->value[0])) {
+			printf("%c", curr->value[0]);
+			l_length += 1;
+		}
+		else {
+			putchar(' ');
+			if (l_length + w_length < MAX_LINE_LENGTH) {
+				l_length += w_length;
+				printf("%s", curr->value);
+			}
+			else {
+				l_length = w_length;
+				printf("\n%s", curr->value);
+			}
+		}
+
+		curr = curr->next;
+	}
+}
+
+bool isPunctuation(char c) {
+	switch (c) {
+		case ',': case ';': case ':': case '.': case '/': case '!': case '?': return true;
+		default: return false;
+	}
 }
 
